de72df911f7bae6fc0deca03cbb03ee0793db306
Mauricio López <maurilo934@hotmail.com>
2022-09-23 09:26:59 -0500

    Añadido detalle paciente

--- authApp/views/__init__.py
+++ authApp/views/__init__.py
@@ -4,3 +4,4 @@ from .vistaCrearUsuario import VistaCrearUsuario
 from .vistaFamiliar import vistaFamiliar
 from .vistaHistoriaClin import vistaHistoriaClin
 from .vistaSignos import vistaSignos
+from .vistaDetallePaciente import VistaDetallePaciente
