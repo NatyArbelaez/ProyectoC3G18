9622b190bdb6ad139a0abe7f04479b2135453874
Mauricio López <maurilo934@hotmail.com>
2022-09-23 12:26:09 -0500

    actualizacion admin

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models.paciente import Paciente
 from .models.perSalud import PersonalSalud
-from .models.usuario import (Usuario)
+from .models.usuario import Usuario
+from .models.familiar import Familiar
+from .models.hisClinica import HistoriaClinica
+from .models.sigVitales import SignosVitales
 
 admin.site.register(Paciente)
 admin.site.register(PersonalSalud)
 admin.site.register(Usuario)
+admin.site.register(Familiar)
+admin.site.register(HistoriaClinica)
+admin.site.register(SignosVitales)
